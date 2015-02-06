8b9fb639c5f44329525ee7f8d7f41ff1bf31db82
Kipp Bowen <jkb@axiosengineering.com>
2015-02-06 17:12:09 -0500

    updated major version for upcoming api changes

--- build.sh
+++ build.sh
@@ -22,9 +22,9 @@ if [ "$1" = "rpm" ]; then
     if [ -e fcalc.spec ]; then
         mydir=`dirname $0`
         tmpdir=`mktemp -d`
-        cp -r ${mydir} ${tmpdir}/fcalc-1.0.1
-        tar czf ${tmpdir}/fcalc-1.0.1.tar.gz --exclude=".svn" -C ${tmpdir} fcalc-1.0.1
-        rpmbuild -ta ${tmpdir}/fcalc-1.0.1.tar.gz
+        cp -r ${mydir} ${tmpdir}/fcalc-2.0.0
+        tar czf ${tmpdir}/fcalc-2.0.0.tar.gz --exclude=".svn" -C ${tmpdir} fcalc-2.0.0
+        rpmbuild -ta ${tmpdir}/fcalc-2.0.0.tar.gz
         rm -rf $tmpdir
     else
         echo "Missing RPM spec file in" `pwd`
--- fcalc.spec
+++ fcalc.spec
@@ -28,7 +28,7 @@ Prefix:         %{_prefix}
 %define _infodir       %{_prefix}/info
 
 Name:           fcalc
-Version:        1.0.1
+Version:        2.0.0
 Release:        1%{?dist}
 Summary:        Component %{name}
 
